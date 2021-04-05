966228482083785a5f4f6e83fee4b562397bc067
krishnendu2007 <74901025+krishnendu2007@users.noreply.github.com>
2021-04-05 23:09:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    maam it is showing me this http://worldtimeapi.org/
+}
